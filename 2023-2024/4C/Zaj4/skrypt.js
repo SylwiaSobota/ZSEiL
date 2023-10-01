4c0a6367b0cc43b7ad801a4750cbb8bd4d283c19
SylwiaSobota <sylwia2238@gmail.com>
2023-10-01 17:40:22 +0200

    Create skrypt.js

---
+++ 2023-2024/4C/Zaj4/skrypt.js
@@ -0,0 +1,10 @@
+const akapit = document.getElementById("imie")
+akapit.style.color = "red"
+
+const akapit1 = document.querySelector("#imi")
+akapit1.style.color = "red"
+
+let da = new Date();
+document.write(da.toLocaleString())
+
+
